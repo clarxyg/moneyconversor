45823a8307367848c43de7d6d390263a5c5d02a3
Clara <clarxyg@gmail.com>
2021-03-22 22:14:05 -0300

    Update convert.js

--- convert.js
+++ convert.js
@@ -1,8 +1,9 @@
 var valueInDolarText = prompt("Qual o valor em dólar que você quer converter?")
 
-var valueInDolarNumber = parseInt(valueInDolarText)
+var valueInDolarNumber = parseFloat(valueInDolarText)
 
 var valueInReal = valueInDolarNumber * 5.51 
 var real = "R$"
+var toFixed = valueInReal.toFixed(2)
 
-alert(real + valueInReal)
+alert(real + toFixed)
